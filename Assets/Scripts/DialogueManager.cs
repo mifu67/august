1517bfa4b0e69d624b5b4af0cd0b822b9921b210
mifu67 <mechllfu@gmail.com>
2023-08-26 20:50:03 -0700

    initial game

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using UnityEngine;
+using Ink.Runtime;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{   
+    [SerializeField]
+    private TextAsset inkFile;
+
+    [SerializeField]
+    private TextMeshProUGUI message;
+    
+    [SerializeField]
+    private float textSpeed;
+    private bool isAddingRichTextTag = false;
+    static Story story;
+    // Start is called before the first frame update
+    void Start()
+    {
+        story = new Story(inkFile.text);
+        message.text = string.Empty;
+        AdvanceDialogue();
+    }
+
+    // Dialogue ends
+    private void FinishDialogue() {
+        Debug.Log("End of Dialogue.");
+    }
+
+    // Advance through the text 
+    void AdvanceDialogue() {
+        string currentSentence = story.Continue();
+        StartCoroutine(TypeSentence(currentSentence));
+    }
+
+    IEnumerator TypeSentence(string sentence) 
+    {
+        message.text = sentence;
+        message.maxVisibleCharacters = 0;
+        foreach (char letter in sentence.ToCharArray()) {
+            if (letter == '<' || isAddingRichTextTag) {
+                isAddingRichTextTag = true;
+                if (letter == '>') {
+                    isAddingRichTextTag = false;
+                }
+            }
+            else {
+                message.maxVisibleCharacters ++;
+                yield return new WaitForSeconds(textSpeed);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (story.canContinue)
+            {
+                AdvanceDialogue();
+            } else {
+                FinishDialogue();
+            }
+        }
+    }
+}
---
+++ Assets/august-script.ink
@@ -0,0 +1,15 @@
+<b>December 11th, 2023. 3:19 A.M.</b>
+Ugh... hello?
+<i>Erika, it's Adrian.</i>
+Adrian? What's going on? It's so late.
+...
+Hello? Are you there?
+<i>It's... it's August. He's dead.</i>
+What?
+<i>We just discovered his body in Fritz Schlegel Park.</i>
+<i>I'm so sorry, Erika.</i>
+...
+...
+I'm dreaming. This isn't real.
+<i>Wait—</i>
+Goodbye, Adrian.
