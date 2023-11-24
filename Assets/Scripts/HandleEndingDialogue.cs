4c138349f77959162768df31923336e9528f44e9
Michelle Fu <mechllfu@gmail.com>
2023-11-24 13:52:46 -0800

    hopefully fixed odd bug

--- Assets/Scripts/HandleEndingDialogue.cs
+++ Assets/Scripts/HandleEndingDialogue.cs
@@ -6,16 +6,17 @@ public class HandleEndingDialogue : MonoBehaviour
 {
     [Header("Ink JSON")]
     [SerializeField] private TextAsset inkJSON;
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (MainManager.Instance.mysterySolved)
+    private void Update(){
+        if (InputManager.GetInstance().GetInteractPressed() && !DialogueManager.GetInstance().dialogueIsPlaying)
         {
-            DialogueManager.GetInstance().EnterDialogueModeEvent(inkJSON, "good_ending", "Feedback");
-        }
-        else
-        {
-            DialogueManager.GetInstance().EnterDialogueModeEvent(inkJSON, "bad_ending", "Feedback");
+            if (MainManager.Instance.mysterySolved)
+            {
+                DialogueManager.GetInstance().EnterDialogueModeEvent(inkJSON, "good_ending", "Feedback");
+            }
+            else
+            {
+                DialogueManager.GetInstance().EnterDialogueModeEvent(inkJSON, "bad_ending", "Feedback");
+            }
         }
     }
 }
